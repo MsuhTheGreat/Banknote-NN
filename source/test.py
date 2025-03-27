2f2a63627f12cfab5d9e29a52e93559d1e285b66
MsuhTheGreat <m.usmantwpsc@gmail.com>
2025-03-27 15:21:59 +0500

    Removed .npy files from Git tracking

--- source/test.py
+++ source/test.py
@@ -1,15 +1,16 @@
 import numpy as np
 from os.path import join
-from sklearn.metrics import f1_score
 
 DATA_DIR = "data"
 MODELS_DIR = "models"
 LAYERS = 3
 
+# Load the necessary variables
 X_test = np.load(join(DATA_DIR, "X_test.npy"))
 Y_test = np.load(join(DATA_DIR, "Y_test.npy"))
 J = np.load(join(MODELS_DIR, "J.npy"))
 
+# Using dictionaries to regulate variables
 W_dict = {}
 b_dict = {}
 Z_dict = {}
@@ -18,6 +19,7 @@ for i in range(1, LAYERS+1):
     W_dict[f"W{i}"] = np.load(join(MODELS_DIR, f"W{i}.npy"))
     b_dict[f"b{i}"] = np.load(join(MODELS_DIR, f"b{i}.npy"))
 
+# Forward Propagation
 for l in range(1, LAYERS+1):
     Z_dict[f"Z{l}"] = np.dot(W_dict[f"W{l}"], A_dict[f"A{l-1}"]) + b_dict[f"b{l}"]
     if l == LAYERS:
@@ -25,10 +27,12 @@ for l in range(1, LAYERS+1):
     else:
         A_dict[f"A{l}"] = (np.exp(Z_dict[f"Z{l}"]) - np.exp(-Z_dict[f"Z{l}"])) / (np.exp(Z_dict[f"Z{l}"]) + np.exp(-Z_dict[f"Z{l}"]))
 
+# Accuracy calculation using my own custom formula
 J_sigmoid = 1 / (1 + np.exp(-J))
 my_accuracy = (1 - J_sigmoid) * 200
 print(f"Accuracy According To My Custom Formula: {my_accuracy}")
 
+# Accuracy according to traditional method
 Y_hat = A_dict[f"A{LAYERS}"]
 predictions = (Y_hat >= 0.5).astype(int)
 traditional_accuracy = np.mean(predictions == Y_test) * 100
--- source/train.py
+++ source/train.py
@@ -2,36 +2,44 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 from os.path import join
+import random
 
+# Set seed for reproducibility
+np.random.seed(42)
+random.seed(42)
+
+# Get the banknote data
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00267/data_banknote_authentication.txt"
 columns = ["Variance", "Skewness", "Kurtosis", "Entropy", "Class"]
 df = pd.read_csv(url, header=None, names=columns)
 
+# Seperate data into input and output
 X = df.iloc[:, :-1].values
 Y = df.iloc[:, -1].values
 
-# X = X.T
-# Y = Y.reshape(1, -1)
-
+# Split the data into training and testing data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
+# Reshape for ease
 X_train, X_test = X_train.T, X_test.T
 Y_train, Y_test = Y_train.reshape(1, -1), Y_test.reshape(1, -1)
 
 DATA_DIR = "data"
 MODELS_DIR = "models"
 
+# Saving data that has to be used for testing later
 np.save(join(DATA_DIR, "X_test.npy"), X_test)
 np.save(join(DATA_DIR, "Y_test.npy"), Y_test)
 
 LAYERS = 3
 ALPHA = 0.001
-EPOCHES = 10_000
+EPOCHES = 100_000
 n0, m = X_train.shape
 n1 = 4
 n2 = 3
 n3 = 1
 
+# Using dictionaries to regulate variables as for loops will be used
 W_dict = {
     "W1": np.random.randn(n1, n0) * np.sqrt(1 / n0),
     "W2": np.random.randn(n2, n1) * np.sqrt(1 / n1),
@@ -78,17 +86,21 @@ for i in range(EPOCHES):
     Y_hat = A_dict[f"A{LAYERS}"]
     J = -1/m * np.sum(Y_train * np.log(Y_hat) + (1-Y_train)*np.log(1-Y_hat))
 
-    if i % 100 == 0:
+    # Keep getting updated on the progress
+    if i % 1000 == 0:
         print(f"Epochs: {i}, Cost: {J}")
 
+# Saving Loss to be used later in accuracy calculaion
 np.save(join(MODELS_DIR, "J.npy"), J)
 
+# Save weights
 c = 1
 for i in W_dict:
     print(f"Final W{c}: {W_dict[i]}")
     np.save(join(MODELS_DIR, f"W{c}.npy"), W_dict[i])
     c += 1
 
+# Save bias
 c = 1
 for i in b_dict:
     print(f"Final b{c}: {b_dict[i]}")
