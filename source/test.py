7d68abf5fd92331a1f56274ec56233b24ef99dac
MsuhTheGreat <m.usmantwpsc@gmail.com>
2025-03-27 10:44:37 +0500

    almost completed the project

---
+++ source/test.py
@@ -0,0 +1,34 @@
+import numpy as np
+from os.path import join
+
+DATA_DIR = "data"
+MODELS_DIR = "models"
+LAYERS = 3
+
+X_test = np.load(join(DATA_DIR, "X_test.npy"))
+Y_test = np.load(join(DATA_DIR, "Y_test.npy"))
+J = np.load(join(MODELS_DIR, "J.npy"))
+
+W_dict = {}
+b_dict = {}
+Z_dict = {}
+A_dict = {"A0": X_test}
+for i in range(1, LAYERS+1):
+    W_dict[f"W{i}"] = np.load(join(MODELS_DIR, f"W{i}.npy"))
+    b_dict[f"b{i}"] = np.load(join(MODELS_DIR, f"b{i}.npy"))
+
+for l in range(1, LAYERS+1):
+    Z_dict[f"Z{l}"] = np.dot(W_dict[f"W{l}"], A_dict[f"A{l-1}"]) + b_dict[f"b{l}"]
+    if l == LAYERS:
+        A_dict[f"A{l}"] = 1 / (1 + np.exp(-Z_dict[f"Z{l}"]))
+    else:
+        A_dict[f"A{l}"] = (np.exp(Z_dict[f"Z{l}"]) - np.exp(-Z_dict[f"Z{l}"])) / (np.exp(Z_dict[f"Z{l}"]) + np.exp(-Z_dict[f"Z{l}"]))
+
+J_sigmoid = 1 / (1 + np.exp(-J))
+my_accuracy = (1 - J_sigmoid) * 200
+print(f"Accuracy According To My Custom Formula: {my_accuracy}")
+
+Y_hat = A_dict[f"A{LAYERS}"]
+predictions = (Y_hat >= 0.5).astype(int)
+traditional_accuracy = np.mean(predictions == Y_test) * 100
+print(f"Accuracy According To Traditional Formula: {traditional_accuracy}")
--- source/train.py
+++ source/train.py
@@ -1,111 +1,91 @@
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import numpy as np
-import os
+from os.path import join
 
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00267/data_banknote_authentication.txt"
+columns = ["Variance", "Skewness", "Kurtosis", "Entropy", "Class"]
+df = pd.read_csv(url, header=None, names=columns)
+
+X = df.iloc[:, :-1].values
+Y = df.iloc[:, -1].values
+
+X = np.reshape(X, (4, -1))
+Y = np.reshape(Y, (1, -1))
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 DATA_DIR = "data"
 MODELS_DIR = "models"
 
+np.save(join(DATA_DIR, "X_test.npy"), X_test)
+np.save(join(DATA_DIR, "Y_test.npy"), Y_test)
 
-def activation_function_hidden(Z):
-    return ( np.exp(Z) - np.exp(-Z) ) / ( np.exp(Z) + np.exp(-Z))
-
-
-def derivative_activation_function_hidden(Z):
-    A = activation_function_hidden(Z)
-    return 1 - np.square(A)
-
-
-(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
-
-X_train = X_train / 255
-X_test = X_test / 255
-
-Y_train = to_categorical(Y_train, num_classes=10)
-Y_test = to_categorical(Y_test, num_classes=10)
-
-X_train = X_train.reshape(28*28, -1)
-X_test = X_test.reshape(28*28, -1)
-Y_train = Y_train.reshape(10, -1)
-Y_test = Y_test.reshape(10, -1)
-
-layers = 3
+LAYERS = 3
+ALPHA = 0.001
+EPOCHES = 10_000
 n0, m = X_train.shape
-n1 = 128
-n2 = 64
-n3 = 10
-
-np.save(os.path.join(DATA_DIR, "X_train.npy"), X_train)
-np.save(os.path.join(DATA_DIR, "Y_train.npy"), Y_train)
-np.save(os.path.join(DATA_DIR, "X_test.npy"), X_test)
-np.save(os.path.join(DATA_DIR, "Y_test.npy"), Y_test)
+n1 = 4
+n2 = 3
+n3 = 1
 
 W_dict = {
     "W1": np.random.randn(n1, n0) * np.sqrt(1 / n0),
     "W2": np.random.randn(n2, n1) * np.sqrt(1 / n1),
-    "W3": np.random.randn(n3, n2) * np.sqrt(1 / n2)
+    "W3": np.random.randn(n3, n2) * np.sqrt(1 / n2),
 }
 b_dict = {
     "b1": np.zeros((n1, 1)),
     "b2": np.zeros((n2, 1)),
-    "b3": np.zeros((n3, 1))
+    "b3": np.zeros((n3, 1)),
 }
 Z_dict = {}
-A_dict = {
-    "A0": X_train
-}
+A_dict = {"A0": X_train}
 dZ_dict = {}
 dW_dict = {}
 db_dict = {}
 
-alpha = 0.001
-epochs = 10_000
-
-for i in range(epochs):
-    
-    for l in range(1, layers+1):
-        W = W_dict[f"W{l}"]
-        A = A_dict[f"A{l-1}"]
-        b = b_dict[f"b{l}"]
-        Z_dict[f"Z{l}"] = np.dot(W, A) + b
-        if l == layers:
-            A_dict[f"A{l}"] = np.exp(Z_dict[f"Z{l}"]) / np.sum(np.exp(Z_dict[f"Z{l}"]), axis=0, keepdims=True)
+for i in range(EPOCHES):
+    # Forward Propagation
+    for l in range(1, LAYERS+1):
+        Z_dict[f"Z{l}"] = np.dot(W_dict[f"W{l}"], A_dict[f"A{l-1}"]) + b_dict[f"b{l}"]
+        if l == LAYERS:
+            A_dict[f"A{l}"] = 1 / (1 + np.exp(-Z_dict[f"Z{l}"]))
         else:
-            A_dict[f"A{l}"] = activation_function_hidden(Z_dict[f"Z{l}"])
+            A_dict[f"A{l}"] = (np.exp(Z_dict[f"Z{l}"]) - np.exp(-Z_dict[f"Z{l}"])) / (np.exp(Z_dict[f"Z{l}"]) + np.exp(-Z_dict[f"Z{l}"]))
     
-    dZ_dict[f"dZ{layers}"] = A_dict[f"A{layers}"] - Y_train
-    dW_dict[f"dW{layers}"] = 1/m * np.dot(dZ_dict[f"dZ{layers}"], A_dict[f"A{layers-1}"].T)
-    db_dict[f"db{layers}"] = 1/m * np.sum(dZ_dict[f"dZ{layers}"], axis=1, keepdims=True)
+    # Backward Propagation for the output layer
+    dZ_dict[f"dZ{LAYERS}"] = A_dict[f"A{LAYERS}"] - Y_train
+    dW_dict[f"dW{LAYERS}"] = 1/m * np.dot(dZ_dict[f"dZ{LAYERS}"], A_dict[f"A{LAYERS-1}"].T)
+    db_dict[f"db{LAYERS}"] = 1/m * np.sum(dZ_dict[f"dZ{LAYERS}"], axis=1, keepdims=True)
+    # Update gradients for output layer
+    W_dict[f"W{LAYERS}"] -= ALPHA * dW_dict[f"dW{LAYERS}"]
+    b_dict[f"b{LAYERS}"] -= ALPHA * db_dict[f"db{LAYERS}"]
 
-    for l in range(layers-1, 0, -1):
-        W = W_dict[f"W{l+1}"].T
-        dZ = dZ_dict[f"dZ{l+1}"]
-        A = A_dict[f"A{l-1}"]
-        Z = Z_dict[f"Z{l}"]
-        dZ_dict[f"dZ{l}"] = np.dot(W, dZ) * derivative_activation_function_hidden(Z)
-        dW_dict[f"dW{l}"] = 1/m * np.dot(dZ_dict[f"dZ{l}"], A.T)
+    # Backward propagation for the rest of layers
+    for l in range(LAYERS-1, 0, -1):
+        dZ_dict[f"dZ{l}"] = np.dot(W_dict[f"W{l+1}"], dZ_dict[f"dZ{l+1}"]) * (1 - np.square(A_dict[f"A{l}"]))
+        dW_dict[f"dW{l}"] = 1/m * np.dot(dZ_dict[f"dZ{l}"], A_dict[f"A{l-1}"].T)
         db_dict[f"db{l}"] = 1/m * np.sum(dZ_dict[f"dZ{l}"], axis=1, keepdims=True)
+        # Update gradients side by side
+        W_dict[f"W{l}"] -= ALPHA * dW_dict[f"dW{l}"]
+        b_dict[f"b{l}"] -= ALPHA * db_dict[f"db{l}"]
     
-    for l in range(1, layers+1):
-        W_dict[f"W{l}"] = W_dict[f"W{l}"] - alpha * dW_dict[f"dW{l}"]
-        b_dict[f"b{l}"] = b_dict[f"b{l}"] - alpha * db_dict[f"db{l}"]
-    
-    J = -1/m * np.sum(Y_train * np.log(A_dict[f"A{layers}"]))
+    # Calculate Loss
+    Y_hat = A_dict[f"A{LAYERS}"]
+    J = -1/m * np.sum(Y_train * np.log(Y_hat) + (1-Y_train)*np.log(1-Y_hat))
 
     if i % 100 == 0:
         print(f"Epochs: {i}, Cost: {J}")
-    
-print("Final Weights:-")
+
+np.save(join(MODELS_DIR, "J.npy"), J)
+
 c = 1
 for i in W_dict:
-    print(f"\tW{c}: {W_dict[i]}")
-    np.save(os.path.join(MODELS_DIR, f"W{c}.npy"), f"W{c}")
-    c += 1
+    print(f"Final W{c}: {W_dict[i]}")
+    np.save(join(MODELS_DIR, f"W{c}.npy"), W_dict[i])
 
-print("Final Bias:-")
 c = 1
 for i in b_dict:
-    print(f"\tb{c}: {b_dict[i]}")
-    np.save(os.path.join(MODELS_DIR, f"b{c}.npy"), f"b{c}")
-    c += 1
+    print(f"Final b{c}: {b_dict[i]}")
+    np.save(join(MODELS_DIR, f"b{c}.npy"), b_dict[i])
